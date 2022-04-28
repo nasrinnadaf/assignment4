4544f8b0e24c757800a9202c2db3fed50647692e
nasrinnadaf <102619662+nasrinnadaf@users.noreply.github.com>
2022-04-28 16:40:31 +0430

    Delete tamrin7.py

--- tamrin7.py
+++
@@ -1,6 +0,0 @@
-first_number = int(input("put first number :"))
-second_number = int(input("put second number :"))
-for i in range ( second_number , first_number*second_number+1):
-    if i % first_number == 0 and i % second_number ==0 :
-        kmm = i
-print(kmm)
